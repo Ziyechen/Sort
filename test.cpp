d06236f36c76cad95304334784c42a4e202c1c49
Ziyechen <18802956884@163.com>
2016-06-04 00:18:28 +0800

    yuan

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdlib.h>
+#include "Sort.h"
+#include "QuickSort.h"
+
+int main()
+{
+	//test();
+	TestQuickSort();
+
+	system("pause");
+	return 0;
+}
